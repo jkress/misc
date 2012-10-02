d226bbbc830a10488bd176ca451bcb450ff54f0e
Jason <jason@Jasons-MacBook-Pro-2.local>
2012-10-02 16:53:26 -0700

    Shell sort working, more random stuff

--- sorting.c
+++ sorting.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <unistd.h>
 #include <sys/time.h>
 
@@ -14,16 +15,28 @@
 int n_compares = 0;
 int n_swaps = 0;
 double start_time;
-char *algorithm = "None";
+double tick_time;
+char *algorithm = "---";
+struct timeval tim;
 
 void reset_stats() {
   n_compares = n_swaps = 0;
 }
 
+void do_sleep() {
+  double elapsed;
+
+  gettimeofday(&tim, NULL);
+  elapsed = (tim.tv_sec+(tim.tv_usec/1000000.0)) - tick_time;
+
+  usleep(LOOP_INTERVAL - elapsed);
+  gettimeofday(&tim, NULL);
+  tick_time = tim.tv_sec+(tim.tv_usec/1000000.0);
+}
+
 void print_data(int *values, int first, int second, int pivot, int color) {
   int i, j; 
 
-  struct timeval tim;
   double cur_time; 
 
   gettimeofday(&tim, NULL);
@@ -35,25 +48,22 @@ void print_data(int *values, int first, int second, int pivot, int color) {
 
   for(i=0; i<ARRAY_SIZE; i++) {
     if(i == first || i == second) {
-      printf("\033[%dm%3d: \033[0m", color, i+1);
+      printf("\033[%dm%3d -> \033[0m|", color, i+1);
     } else {
-      printf("%3d: ", i+1);
+      printf("%3d    |", i+1);
     }
 
-    if(i == pivot) {
-      printf("\033[%dm", COLOR_MAGENTA);
-    }
+    if(i == pivot) printf("\033[%dm", COLOR_MAGENTA);
 
-    for(j=0; j<values[i]; j++) {
-      printf("-");
-    }
-    printf(">");
-    if(i == pivot) {
-      printf("\033[0m");
-    }
+    for(j=0; j<=values[i]; j++) printf("-");
+
+    printf("|");
+
+    if(i == pivot) printf("\033[0m"); 
 
     printf("\n");
   }
+  do_sleep();
 }
 
 void color_swap(int *values, int first, int second, int pivot) {
@@ -66,26 +76,23 @@ void color_swap(int *values, int first, int second, int pivot) {
   values[second] = tmp;
 
   print_data(values, first, second, pivot, COLOR_GREEN);
-  usleep(LOOP_INTERVAL);
 }
 
 void color_noswap(int *values, int first, int second, int pivot) {
   print_data(values, first, second, pivot, COLOR_RED);
-  usleep(LOOP_INTERVAL);
 }
 
-int color_compare(int *values, int first, int second, int pivot) {
+void color_compare(int *values, int first, int second, int pivot) {
   n_compares++;
 
   print_data(values, first, second, pivot, COLOR_YELLOW);
-  usleep(LOOP_INTERVAL);
 }
 
 void bubble_sort(int *values) {
   int i, j;
   int newn = -1;
 
-  algorithm = "Bubblesort";
+  algorithm = "Bubble Sort";
 
   for(i=ARRAY_SIZE-1; i>0; i--) {
     for(j=0; j<i; j++) {
@@ -139,7 +146,7 @@ void quick_sort_r(int *values, int left, int right) {
 }
 
 void quick_sort(int *values) {
-  algorithm = "Quicksort";
+  algorithm = "Quick Sort";
 
   quick_sort_r(values, 0, ARRAY_SIZE - 1);
 }
@@ -169,7 +176,7 @@ void heap_sort_siftDown(int *values, int start, int end) {
       color_swap(values, root, swap, end+1);
       root = swap;
     } else {
-      color_noswap(values, root, swap, end+1);
+      color_noswap(values, child, child+1, end+1);
       return;
     }
   }
@@ -188,7 +195,7 @@ void heap_sort_heapify(int *values) {
 void heap_sort(int *values) {
   int end = ARRAY_SIZE - 1;
 
-  algorithm = "Heapsort";
+  algorithm = "Heap Sort";
 
   heap_sort_heapify(values);
 
@@ -202,7 +209,7 @@ void heap_sort(int *values) {
 void insertion_sort(int *values) {
   int i, j;
 
-  algorithm = "Insertion";
+  algorithm = "Insertion Sort";
 
   for(i = 1; i < ARRAY_SIZE; i++) {
     for(j = i; j > 0; j--) {
@@ -221,7 +228,7 @@ void selection_sort(int *values) {
   int i, j;
   int iMin;
 
-  algorithm = "Selection";
+  algorithm = "Selection Sort";
 
   for(j = 0; j < ARRAY_SIZE-1; j++) {
     iMin = j;
@@ -238,6 +245,30 @@ void selection_sort(int *values) {
     }
   }
 
+}
+
+
+void shell_sort(int *values) {
+  int gap, i, j, k;
+  int n = ARRAY_SIZE;
+  int gaps[] = {48, 21, 7, 5, 1};
+
+  algorithm = "Shell Sort";
+
+  for( k = 0; k < sizeof(gaps) / sizeof(int); k++ ) {
+    gap = gaps[k];
+    for( i = 0 ; i < n ; i++ ) {
+      for( j = i; j >= gap; j -= gap ) {
+        color_compare(values, j, j-gap, i);
+        if(values[j] < values[j-gap]) {
+          color_swap(values, j, j-gap, i);
+        } else {
+          color_noswap(values, j, j-gap, i);
+          break;
+        }
+      }
+    }
+  }
 }
 
 void generate_data(int *values) {
@@ -260,20 +291,24 @@ void generate_data(int *values) {
 
 int main() {
   int i, j;
+  int max_l;
   int values[ARRAY_SIZE];
-
-  struct timeval tim;
-  gettimeofday(&tim, NULL);
-  srand(tim.tv_usec / 1.0);
+  char ctrl_str[80];
 
   typedef void (*fn_ptr)( int* );
   fn_ptr fn_ptr_array[] = {
+    &heap_sort,
+    &quick_sort,
     &selection_sort,
     &insertion_sort,
-    &quick_sort,
-    &heap_sort,
+    &shell_sort,
     &bubble_sort,
   };
+
+  gettimeofday(&tim, NULL);
+  srand(tim.tv_usec / 1.0);
+
+  start_time = tick_time = tim.tv_sec+(tim.tv_usec/1000000.0);
 
   int array_size = sizeof(fn_ptr_array) / sizeof(fn_ptr);
   
@@ -299,6 +334,8 @@ int main() {
     gettimeofday(&tim, NULL);
     end_time = tim.tv_sec+(tim.tv_usec/1000000.0);
     
+    print_data(values, -1, -1, -1, -1);
+
     struct result current_result;
 
     current_result.algorithm = algorithm;
@@ -313,11 +350,19 @@ int main() {
     i++;
 
     if(i == array_size) {
+      max_l = 0;
+      for(j=0; j < array_size; j++) {
+        if( strlen(results_list[j].algorithm) > max_l) {
+            max_l = strlen(results_list[j].algorithm);
+        }
+      }
       system("clear");
       printf("Results:\n\n");
 
+      sprintf(ctrl_str, "Algorithm: %%%ds  Compares: %%4d  Swaps: %%3d  Time: %%3.2fs\n", -max_l);
+
       for(j = 0; j < array_size; j++) {
-        printf("Algorithm: \"%s\"\tCompares: %3d  Swaps: %3d  Time: %3.2fs\n",
+        printf(ctrl_str,
                results_list[j].algorithm,
                results_list[j].n_compares,
                results_list[j].n_swaps,
